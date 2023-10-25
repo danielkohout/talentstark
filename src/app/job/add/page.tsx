30662d18415ba18973471b075dad2f81ab661dd5
danielkohout <danielkohout@icloud.com>
2023-10-25 19:08:34 +0200

    update

--- src/app/job/add/page.tsx
+++ src/app/job/add/page.tsx
@@ -1,40 +1,7 @@
-import AddProcess from "@/components/job/add/AddProcess";
-import BasicJobInformation from "@/components/job/add/BasicJobInformation";
-import { serverTrpc } from "@/routers/trpc-caller";
+import React from "react";
 
-const page = async () => {
-  const data = await serverTrpc.getUser();
-  const addJob = async (formData: FormData) => {
-    "use server";
-    const jobName = formData.get("jobName") as string;
-    if (!jobName) {
-      console.log("Kein Name angegeben");
-      return;
-    }
-    await serverTrpc.jobRouter.addJob({ text: jobName });
-  };
-  return (
-    <>
-      <AddProcess />
-      <div className="py-24">
-        <div className="max-w-4xl mx-auto px-6 lg:px-8">
-          <div className="text-center">
-            <h1 className="font-bold text-2xl lg:text-4xl">
-              Zeit eine Stelle zu besetzen
-            </h1>
-            <p className="mt-2 text-gray-600">
-              Erstelle einen neuen Job in wenigen Schritten.
-            </p>
-          </div>
-          <div className="bg-white rounded-2xl mt-10">
-            <div className="p-8">
-              <BasicJobInformation addJob={addJob} />
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
-  );
+const page = () => {
+  return <div>page</div>;
 };
 
 export default page;
