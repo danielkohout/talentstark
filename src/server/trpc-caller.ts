8cf469ff24e8e95841d1f5b982cb1b99985886ae
danielkohout <danielkohout@icloud.com>
2023-10-29 06:47:59 +0100

    trpc server old way

--- src/server/trpc-caller.ts
+++
@@ -1,6 +0,0 @@
-import "server-only";
-import { appRouter } from "./index";
-export const serverTrpc = appRouter.createCaller({
-  eventServer: { trigger: async () => {} },
-  session: { user: null },
-});
