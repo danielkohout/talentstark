d0c5489766afe2274167bfd4bd61d553a87c8ee4
danielkohout <danielkohout@icloud.com>
2023-10-30 09:12:14 +0100

    server session login

--- src/app/(public)/auth/verify/page.tsx
+++ src/app/(public)/auth/verify/page.tsx
@@ -1,10 +1,10 @@
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 import VerifyLogin from "@/components/user/VerifyLogin";
-import { getServerSession } from "next-auth";
+import { getServerSession } from "next-auth/next";
 import { redirect } from "next/navigation";
 
 const page = async () => {
-  const session = getServerSession;
+  const session = await getServerSession(authOptions);
 
   if (!session) {
     return <VerifyLogin />;
